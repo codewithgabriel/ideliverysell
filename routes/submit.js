a9da777dc01cd307b30046e3f65b1e716ce81a8d
unknown <olatoyeolaide@gmail.com>
2023-09-08 02:42:43 -0700

    update phone number

--- routes/submit.js
+++ routes/submit.js
@@ -4,13 +4,13 @@ import client from './dbconfig.js';
 
 
 export default router.use("/" ,  async function (req, res , next) {
-    const {fullname , email , addressOne , addressTwo , city , state , zipcode } = req.body;
+    const  { phonenumber , fullname , email , addressOne , addressTwo , city , state , zipcode } = req.body;
      
     try{
         //check if user already exists
         let user =  await client.collection('applicants').findOne({email: email});
         if (user) throw({error: true , message: "Applicant Already exists."});
-        let response = client.collection('applicants').insertOne( {email , fullname , addressOne , addressTwo , city , state , zipcode , document_uri: [], ssn: ''});
+        let response = client.collection('applicants').insertOne( { phonenumber , email , fullname , addressOne , addressTwo , city , state , zipcode , document_uri: [], ssn: ''});
         res.render("submit-success" , {
             error: false ,
              message :"Thank you for submitting your application through our platform. We want to inform you that your application has been successfully received. Our company's dedicated team will carefully review your application and qualifications. If your profile aligns with our requirements, we will be in touch with you shortly to initiate the next steps in the application process. We appreciate your interest in joining our team and look forward to the possibility of working together. In the meantime, feel free to reach out if you have any questions or require additional information. Good luck!" , 
